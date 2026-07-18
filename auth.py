17b2bc843df901b953d739d9f8800dd1154e67a6
elisemarten <elisemarten@proton.me>
2026-07-18 22:41:12 +0300

    Implement AuthService with login method

--- auth.py
+++ auth.py
@@ -1,1 +1,44 @@
-import jwt
+from repository import UserRepository
+
+from password import PasswordVerifier
+
+from jwt_service import JwtService
+
+
+class AuthService:
+
+    def login(
+
+        self,
+
+        username,
+
+        password
+
+    ):
+
+        user = UserRepository().find(
+
+            username
+
+        )
+
+        if not user:
+
+            return None
+
+        if not PasswordVerifier().check(
+
+            password,
+
+            user.password
+
+        ):
+
+            return None
+
+        return JwtService().create(
+
+            username
+
+        )
